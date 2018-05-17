33afbc4291e113f9d36376f415c5d5dafc60a6b7
Sammy Wu <swucode@gmail.com>
2018-05-17 08:05:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 function produceDrivingRange() {
-  
+
 }
 
 function produceTipCalculator(tip) {
   return function(charge) {
     return charge * tip;
+  }
+}
+
+function createDriver() {
+  let driverId = 0;
+  return class {
+    constructor(name) {
+      this.name = name;
+      this.id = ++driverId;
+    }
   }
 }
