7ef2cfd6e003420246f63ca71603b37c7367c18c
Sammy Wu <swucode@gmail.com>
2018-05-17 08:05:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+function produceDrivingRange(blockRange) {
+  return function(start, end) {
+    result = blockRange - (parseInt(end) - parseInt(start));
+    if (result < 0) {
+      return `${Math.abs(result)} blocks out of range`;
+    } else {
+      return `within range by ${result}`;
+    };
+  };
+};
+
 function produceDrivingRange() {
 
 }
